034f3451532ec8fa4a1f89c507b2ffa95cee7d37
MrKitten <thekremlin@protonmail.com>
2018-02-05 09:25:45 -0500

    added actual text to the messages table derp

--- db/migrate/20180205002341_create_messages.rb
+++ db/migrate/20180205002341_create_messages.rb
@@ -4,6 +4,7 @@ class CreateMessages < ActiveRecord::Migration[5.1]
       t.string :tinder_id
       t.string :to_tinder_id
       t.string :from_tinder_id
+      t.string :text
       t.string :type
       t.datetime :tinder_timestamp
 
