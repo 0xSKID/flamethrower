76a81d0c27d7cefed5ccb760588a932ad963c066
MrKitten <thekremlin@protonmail.com>
2018-02-08 12:54:12 -0500

    added tests for updates worker

---
+++ spec/workers/updates_worker_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+RSpec.describe 'UpdatesWorker' do
+  let(:account) do
+    Account.create
+  end
+
+  let(:client) do
+    instance_double(Tinder::Client, updates: updates)
+  end
+
+  let(:updates) do
+    {
+      'matches' => {
+        'person' => {},
+        'messages' => []
+      }
+    }
+  end
+
+  before do
+    expect(Tinder::Client).to receive(:new).and_return(client)
+    expect(client).to receive(:updates).with(account.updates.last&.pluck(:created_at)&.iso8601).and_return(updates)
+  end
+
+  describe 'perform' do
+    it 'creates an update model' do
+      expect { UpdatesWorker.new.perform(account.id) }
+        .to change { Update.count }.by(1)
+    end
+
+    it 'creates a raw data model' do
+      expect { UpdatesWorker.new.perform(account.id) }
+        .to change { RawData.count }.by(1)
+    end
+
+    it 'sends a messages to process updates worker' do
+      expect(ProcessUpdatesWorker).to receive(:perform_async)
+      UpdatesWorker.new.perform(account.id)
+    end
+  end
+end
