d2a556401a04184387da9aa37d7e5d36f98c25a5
MrKitten <thekremlin@protonmail.com>
2018-02-12 21:37:53 -0500

    Learning react is annoying, might revert this later

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
+const userIsAuthenticated = connectedRouterRedirect({
+  redirectPath '',
+  authenticatedSelector: state => state.auth.data.id != null,
+  wrapperDisplayName: 'UserIsAUthenticated'
+});
+
+const userIsNotAuthenticated = connectedRouterRedirect({
+  redirectPath: '/swipe'
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Flamethrower</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <div>
+            <Route exact path="" component={userIsNotAuthenticated(LoginPage)} />
+            <Route exact path="" component={userIsAuthenticated(SwipePage)} />
+          </div>
+        </Router>
       </div>
     );
   }
